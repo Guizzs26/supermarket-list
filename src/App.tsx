68f5e5e542bdf9335de1e8518b2c98b4036d674f
Guilherme <guiteixeira1926@gmail.com>
2024-07-05 17:57:17 -0400

    feat: add header component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-function App() {}
+import styles from "./App.module.css";
 
-export default App;
+import Logo from "./components/logo/Logo";
+
+export default function App() {
+  return (
+    <div className={styles.appContainer}>
+      <Logo />
+    </div>
+  );
+}
---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,17 @@
+import styles from "./logo.module.css";
+
+export default function Logo() {
+  return (
+    <header className={styles.header}>
+      <img
+        src="carrinho-de-compras.png"
+        alt="An illustration of a person shopping"
+        className={styles.imgLogo}
+      />
+      <h1 className={styles.primaryHeading}>
+        Shopping <br />
+        Grocery List
+      </h1>
+    </header>
+  );
+}
